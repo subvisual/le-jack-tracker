ffe50777cb6977836a8e6f5a0330e981ca316dc7
David Lange <lange@finiam.com>
2023-01-10 12:12:06 +0000

    style calendar, add links

--- src/lib/components/Month.svelte
+++ src/lib/components/Month.svelte
@@ -9,28 +9,46 @@
 </script>
 
 <h2 class="name">{monthName}</h2>
-<div class="month">
+<section class="month">
   {#each month.days as day}
     <div class="day">
-      {day.current.toLocaleDateString('default', { day: '2-digit' })}
-      {#each day.events as evt}
-        {evt.name}
-      {/each}
+      <p class="day-digit">
+        {day.current.toLocaleDateString('default', { day: '2-digit' })}
+      </p>
+      <div class="events">
+        {#each day.events as evt}
+          <a href={evt.url} target="_blank" rel="noreferrer">{evt.name}</a>
+        {/each}
+      </div>
     </div>
   {/each}
-</div>
+</section>
 
 <style>
   .name {
     display: block;
     margin-bottom: 0.5em;
+    color: tomato;
   }
   .month {
     display: grid;
-    grid-template-columns: repeat(7, 1fr);
+    grid-template-columns: repeat(5, 1fr);
+    gap: 0.5rem 0;
     margin-bottom: 40px;
+    border-bottom: 1px solid lightcoral;
   }
   .day {
-    height: 90px;
+    min-height: 100px;
+    padding-right: 0.5em;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  .day-digit {
+    color: grey;
+  }
+  .events a {
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 </style>
