f257ac9d4fd939084fbcefee9a0148a295de0b72
ZePedroResende <17102689+ZePedroResende@users.noreply.github.com>
2023-01-13 01:55:00 +0000

    Filter events by country

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let countries: String;
+  export let selectedCountry: String;
+  $:console.log(countries)
+</script>
+
+<!-- svelte-ignore a11y-no-onchange -->
+<section class="menu-cont">
+	<select class="menu" 
+					name="menu" 
+					id="menu" 
+					bind:value={selectedCountry}>
+		<option disabled selected value="">Select a country.</option>
+		<option value="all">All countries</option>
+		{#each countries as country}
+			<option value={country}>{country}</option>
+		{/each}
+	</select>
+</section>
+
+
+<style>
+	.menu-cont {
+		width: 40%;
+		display: flex;
+		margin: 0 10px 0 0;
+	}
+	
+	select {
+		margin: 0;
+		font-size: 1.18rem;
+		border: 1px solid gray;
+		border-radius: 5px;
+	}
+	
+	.menu {
+		width: 100%
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,42 @@
   import EventDisplay from '$lib/components/EventDisplay.svelte';
   import Header from '$lib/components/Header.svelte';
   import Month from '$lib/components/Month.svelte';
+  import Menu from '$lib/components/Menu.svelte';
   import eventsCalendar from '$lib/stores/eventsCalendar';
   import type { PageData } from './$types';
+  import { onMount } from 'svelte';
 
   export let data: PageData;
+
+  let countries: any = []; // menu built from bookData
+  let selectedCountry = ''; //  menu selection
+
+  const getcountries = () => {
+    for (let event of data.events) {
+      if (!countries.includes(event.country)) {
+        countries = [...countries, event.country];
+      }
+    }
+    countries = countries.sort();
+  };
+
+  onMount(() => getcountries());
+
+  // Query results
+  let filteredEvents: any = [];
+
+  // For Select Menu
+  $: if (selectedCountry) getEventsByCountry();
+  $: console.log(filteredEvents, selectedCountry);
+
+  const getEventsByCountry = () => {
+    if (selectedCountry === 'all') {
+      return (filteredEvents = []);
+    }
+    return (filteredEvents = data.events.filter(
+      (event) => event.country === selectedCountry
+    ));
+  };
 
   eventsCalendar.create(data.events);
 </script>
@@ -16,6 +48,11 @@
 
 <Header />
 <main class="calendar">
+  <Menu
+    {countries}
+    bind:selectedCountry
+    on:change={() => eventsCalendar.create(filteredEvents)}
+  />
   <h1 class="calendar-title">{$eventsCalendar.year}</h1>
   {#each $eventsCalendar.calendar as month}
     <Month {month} />
